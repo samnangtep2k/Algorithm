5f49406c1a90b6966842c2ade1753746212265c2
Samnang Tep <samnangtep2k@yahoo.com>
2017-06-02 22:57:03 -0700

    Add initial project

---
+++ Algorithm/ArrayTest.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithm
+{
+    [TestClass]
+    public class ArrayTest
+    {
+        [TestMethod]
+        public void SortJaggedArray()
+        {
+            int[][] a =
+            {
+                new int[] { 2,5,1,10,6,2 },
+                new int[] { 10,2,8,100,1 },
+                new int[] {-1,-2},
+            };
+
+            PrintJaggedArray(a);
+            Console.WriteLine("xxxxxxxxxxxxxx");
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                Sort(a[i]);
+            }
+            Sort(a);
+
+            PrintJaggedArray(a);
+        }
+
+        private void Sort(int[][] a)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int j = i + 1; j < a.Length; j++)
+                {
+                    if (a[i][0] > a[j][0])
+                    {
+                        Swap(a, i, j);
+                    }
+                }
+            }
+        }
+
+        private void Swap(int[][] a, int m, int n)
+        {
+            int[] temp = a[m];
+            a[m] = a[n];
+            a[n] = temp;
+        }
+
+        public void Sort(int[] a)
+        {
+            for (int i=0; i<a.Length-1; i++)
+            {
+                for (int j=i+1; j<a.Length; j++)
+                {
+                    if (a[i] > a[j])
+                    {
+                        Swap(a, i, j);
+                    }
+                }
+            }
+        }
+
+        private void PrintJaggedArray(int[][] a)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int j = 0; j < a[i].Length; j++)
+                {
+                    Console.Write("{0} ", a[i][j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private void Swap(int[] a, int i, int j)
+        {
+            int temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+    }
+}
---
+++ Algorithm/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Algorithm")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Algorithm")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9f3b58de-69ae-4cd2-af87-a0faf6fa57db")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
